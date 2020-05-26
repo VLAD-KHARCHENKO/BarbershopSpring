c71adfc3dfaca921d5b0025c190bfd2606bc485a
VLAD-KHARCHENKO <vladuslav.kharchenko.jd@gmail.com>
2020-05-26 13:58:59 +0300

    Initial commit

---
+++ BarbershopSpring/src/main/java/com/BarbershopSpring/BarbershopSpring/BarbershopSpringApplication.java
@@ -0,0 +1,13 @@
+package com.BarbershopSpring.BarbershopSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopSpringApplication.class, args);
+	}
+
+}
---
+++ BarbershopSpring/src/test/java/com/BarbershopSpring/BarbershopSpring/BarbershopSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BarbershopSpring.BarbershopSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
